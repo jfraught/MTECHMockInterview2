d4170fea93d1c9ddede4affd8e7d233aed09090d
Jordan Fraughton <fraughtdawg@gmail.com>
2024-07-19 07:39:43 -0600

    Initial Commit

---
+++ MockInterviewTwo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockInterviewTwo
+//
+//  Created by Jordan Fraughton on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockInterviewTwo/MockInterviewTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockInterviewTwoApp.swift
+//  MockInterviewTwo
+//
+//  Created by Jordan Fraughton on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MockInterviewTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
